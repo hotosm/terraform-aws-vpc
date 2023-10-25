97a886ffe4382d1c740cd2665aba99282601e18c
eternaltyro <eternaltyro@protonmail.ch>
2023-10-25 12:32:25 +0100

    Remove NAT Gateway from private route table
    NAT gateway works over IPv4 and is very expensive since they charge per
    GB data transfered out. Elastic IP, NAT gateway, and external network
    traffic charges together account for a significant portion of AWS
    data-transfer costs.
    
    This module is now biased in favour of IPv6 for network traffic.
    
    - Removed NAT Gateway route in private Route Table.
    - Removed NAT Gateway itself.
    - Removed Elastic IP for NAT Gateway.

--- main.tf
+++ main.tf
@@ -66,15 +66,6 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_eip" "nat" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[1].id
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.primary.id
 }
@@ -103,11 +94,6 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.primary.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
 
   route {
     ipv6_cidr_block        = "::/0"
