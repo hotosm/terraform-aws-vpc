12379b7683649829117397a8aa9796b85588ac76
eternaltyro <eternaltyro@protonmail.ch>
2024-03-27 10:07:49 +0000

    Add IPv4 routing via NAT GW

--- main.tf
+++ main.tf
@@ -136,6 +136,11 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.primary.id
 
   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  route {
     ipv6_cidr_block        = "::/0"
     egress_only_gateway_id = aws_egress_only_internet_gateway.eigw.id
   }
