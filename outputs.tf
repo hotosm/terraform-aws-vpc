c8c1b204610288ae4f8d4df9d4d49335e58fa39b
Nischal Shrestha <39024181+nischalstha9@users.noreply.github.com>
2025-03-14 09:41:51 +0545

    feat: use custom outputs in case you dont want to create resource

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,29 @@
 output "vpc_id" {
-  value = aws_vpc.primary.id
-
+  value       = var.vpc_id
   description = "VPC ID of the primary VPC created by the VPC module"
 }
 
 output "public_subnets" {
-  value = [for subnet in aws_subnet.public : subnet.id]
-
+  value       = var.public_subnets
   description = "List of public subnet IDs"
 }
 
 output "private_subnets" {
-  value = [for subnet in aws_subnet.private : subnet.id]
-
+  value       = var.private_subnets
   description = "List of public subnet IDs"
 }
 
 output "ipv4_prefix_list_id" {
-  value = aws_ec2_managed_prefix_list.admin-v4.id
-
+  value       = var.ipv4_prefix_list_id
   description = "ID of the prefix list with IPv4 addresses; Use this for security group SSH allow-lists"
 }
 
 output "ipv6_prefix_list_id" {
-  value = aws_ec2_managed_prefix_list.admin-v6.id
-
+  value       = var.ipv6_prefix_list_id
   description = "ID of the prefix list with IPv6 addresses; Use this for security group SSH allow-lists"
 }
 
 output "default_security_group_id" {
-  value = aws_default_security_group.default.id
-
+  value       = var.default_security_group_id
   description = "Default Security Group ID for the VPC"
 }
